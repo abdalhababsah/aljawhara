402f7f51392f2218fbcccd8e959332f92578057d
abdalhababsah <abdalhababseh@outlook.com>
2024-12-06 20:49:07 +0300

    dashboard link

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -3,7 +3,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-radius-lg fixed-end me-2 rotate-caret bg-white my-2" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute start-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand px-4 py-3 m-0" href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard " target="_blank">
+      <a class="navbar-brand px-4 py-3 m-0"  target="_blank">
         <span id="title" class="me-1 text-sm text-dark">جوهرة الشرق</span>
       </a>
     </div>
